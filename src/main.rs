d491a34eef6cf2b901d641041739a00960018807
Robert Swain <robert.swain@gmail.com>
2022-08-20 00:33:00 +0200

    Add basic camera and physics

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,71 @@
 use bevy::prelude::*;
+use bevy_rapier3d::prelude::*;
+use smooth_bevy_cameras::{
+    controllers::fps::{FpsCameraBundle, FpsCameraController, FpsCameraPlugin},
+    LookTransformPlugin,
+};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugin(LookTransformPlugin)
+        .add_plugin(FpsCameraPlugin::default())
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
         .add_startup_system(setup)
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn_bundle(Camera2dBundle::default());
-    commands.spawn_bundle(SpriteBundle {
-        texture: asset_server.load("icon.png"),
-        ..Default::default()
-    });
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands
+        .spawn_bundle(Camera3dBundle {
+            transform: Transform::from_xyz(-3.0, 3.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..Default::default()
+        })
+        .insert_bundle(FpsCameraBundle::new(
+            FpsCameraController::default(),
+            Vec3::new(-2.0, 5.0, 5.0),
+            Vec3::new(0., 0., 0.),
+        ));
+
+    commands
+        .spawn_bundle((Collider::cuboid(100.0, 0.1, 100.0),))
+        .insert_bundle(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Box {
+                min_x: -50.0,
+                max_x: 50.0,
+                min_y: -0.05,
+                max_y: 0.05,
+                min_z: -50.0,
+                max_z: 50.0,
+            })),
+            material: materials.add(StandardMaterial {
+                base_color: Color::WHITE,
+                ..default()
+            }),
+            transform: Transform::from_xyz(0.0, -2.0, 0.0),
+            ..default()
+        });
+
+    commands
+        .spawn_bundle((
+            RigidBody::Dynamic,
+            Collider::ball(0.5),
+            Restitution::coefficient(0.7),
+        ))
+        .insert_bundle(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Icosphere {
+                radius: 0.5,
+                ..default()
+            })),
+            material: materials.add(StandardMaterial {
+                base_color: Color::INDIGO,
+                ..default()
+            }),
+            transform: Transform::from_xyz(0.0, 4.0, 0.0),
+            ..default()
+        });
 }
